371e928460e4ddf006c2214a74d69ac6f55878e2
Vlad Eremeev <eremeev.vlad.v@yandex.ru>
2022-06-10 18:02:42 +0300

    Added database and remade simple HTML page with fragments division

--- src/main/java/com/wraithbeam/firstApp/controllers/mainPageController.java
+++ src/main/java/com/wraithbeam/firstApp/controllers/mainPageController.java
@@ -1,14 +1,21 @@
 package com.wraithbeam.firstApp.controllers;
 
+import com.wraithbeam.firstApp.models.Flat;
+import com.wraithbeam.firstApp.repositorys.FlatRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class mainPageController {
+    @Autowired
+    private FlatRepository flatRepository;
+
     @GetMapping("/")
     public String mainpage(Model model) {
-        model.addAttribute("text", "Привет всем, пупси!");
+        Iterable<Flat> flats = flatRepository.findAll();
+        model.addAttribute("flats", flats);
         return "mainpage";
     }
 }
